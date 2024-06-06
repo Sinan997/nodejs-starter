bf744a82def7da51b1abc298075349a995c1c61d
Sinan997 <sinanozturk997@gmail.com>
2024-06-06 19:41:57 +0300

    remove dist

--- dist/constants/roles.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["Admin"] = "admin";
-})(Roles || (exports.Roles = Roles = {}));
--- dist/models/refresh-token.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const RefreshTokenSchema = new mongoose_1.Schema({
-    token: { type: String, required: true },
-    userId: { type: String, required: true },
-});
-exports.default = (0, mongoose_1.model)('RefreshToken', RefreshTokenSchema);
